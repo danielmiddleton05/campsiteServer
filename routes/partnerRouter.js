de4f7683a0fe7ffd3d8dbcb4705f01291462bccf
Daniel Middleton <daniel.middleton05@gmail.com>
2023-06-17 15:00:32 +0900

    Mongoose Pop 2

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,50 +1,75 @@
 const express = require('express');
-const Partner = require('../models/partner');
-
 const partnerRouter = express.Router();
+const Partner = require('../models/partner');
+const authenticate = require('../authenticate');
 
 partnerRouter.route('/')
-.get((req, res, next) => {
-    Partner.find()
-    .then(partners => res.status(200).json(partners))
-    .catch(err => next(err));
-})
-.post((req, res, next) => {
-    Partner.create(req.body)
-    .then(partner => res.status(200).json(partner))
-    .catch(err => next(err));
-})
-.put((req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /partners');
-})
-.delete((req, res, next) => {
-    Partner.deleteMany()
-    .then(partners => res.status(200).json(partners))
-    .catch(err => next(err));
-});
+    .get((req, res, next) => {
+        Partner.find()
+            .then(partners => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partners);
+            })
+            .catch(err => next(err));
+    })
+    .post(authenticate.verifyUser, (req, res, next) => {
+        Partner.create(req.body)
+            .then(partner => {
+                console.log('Campsite Created ', partner);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner);
+            })
+            .catch(err => next(err));
+    })
+    .put(authenticate.verifyUser, (req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /partners');
+    })
+    .delete(authenticate.verifyUser, (req, res, next) => {
+        Partner.deleteMany()
+            .then(response => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(response);
+            })
+            .catch(err => next(err));
+    });
 
 partnerRouter.route('/:partnerId')
-.get((req, res, next) => {
-    Partner.findById(req.params.partnerId)
-    .then(partner => res.status(200).json(partner))
-    .catch(err => next(err));
-})
-.post((req, res) => {
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
-})
-.put((req, res, next) => {
-    Partner.findByIdAndUpdate(req.params.partnerId, {
-        $set: req.body
-    }, { new: true })
-    .then(partner => res.status(200).json(partner))
-    .catch(err => next(err));
-})
-.delete((req, res, next) => {
-    Partner.findByIdAndDelete(req.params.partnerId)
-    .then(partner => res.status(200).json(partner))
-    .catch(err => next(err));
-});
+    .get(authenticate.verifyUser, (req, res, next) => {
+        Partner.findById(req.params.partnerId)
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner);
+            })
+            .catch(err => next(err));
+    })
+    .post(authenticate.verifyUser, (req, res) => {
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /partner/${req.params.partnerId}`);
+    })
+    .put(authenticate.verifyUser, (req, res, next) => {
+        Partner.findByIdAndUpdate(req.params.partnerId, {
+            $set: req.body
+        }, { new: true })
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner);
+            })
+            .catch(err => next(err));
+    })
+    .delete(authenticate.verifyUser, (req, res, next) => {
+        Partner.findByIdAndDelete(req.params.partnerId)
+            .then(response => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(response);
+            })
+            .catch(err => next(err));
+    });
 
 module.exports = partnerRouter;
